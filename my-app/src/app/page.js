54446b1c08f2edb9b8b691166f6271118866ae65
FDMOliveira <filipe.dmo@gmail.com>
2024-07-28 11:54:04 +0000

    changes

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -2,7 +2,7 @@ import FluidAnimation, { defaultConfig } from "../../components/FluidAnimation";
 
 export default function Home() {
   return (
-    <div className="fixed top-0 left-0 bottom-0 right-0 fluid-animation bg-red-400">
+    <div className="fixed top-0 left-0 bottom-0 right-0 fluid-animation bg-red">
       <FluidAnimation config={defaultConfig} />
     </div>
   );
